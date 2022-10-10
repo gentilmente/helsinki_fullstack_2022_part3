3d602d2b2a6cc791fbe2dacc8f3abc8327a89373
gentilmente <egentilemontes@gmail.com>
2022-10-10 16:39:25 -0300

    3.3: Phonebook backend step3

--- index.js
+++ index.js
@@ -18,17 +18,17 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
-/*
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   const person = persons.find((person) => person.id === id);
   if (person) {
-      response.json(person);
+    response.json(person);
   } else {
     response.status(404).end();
   }
 });
 
+/*
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   persons = persons.filter((person) => person.id !== id);
