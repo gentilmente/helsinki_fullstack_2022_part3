28bbef07f768eaed8e07ec39373b6354629a6859
gentilmente <egentilemontes@gmail.com>
2022-10-10 16:41:32 -0300

    3.4: Phonebook backend step4

--- index.js
+++ index.js
@@ -28,14 +28,14 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
-/*
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   persons = persons.filter((person) => person.id !== id);
-  
+
   response.status(204).end();
 });
 
+/*
 const generateId = () => {
   const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
   return maxId + 1;
