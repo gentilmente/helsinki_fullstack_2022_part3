8f997a190cca094297562deef8276a0847028ab5
gentilmente <egentilemontes@gmail.com>
2022-10-12 20:20:09 -0300

    3.12: Command-line database

--- backend/mongo.js
+++ backend/mongo.js
@@ -1,35 +1,41 @@
 const mongoose = require("mongoose");
-
-if (process.argv.length < 5) {
+if (process.argv.length < 3) {
   console.log("Please: node mongo.js <password> <name> <number>");
   process.exit(1);
 }
 
 const password = process.argv[2];
-const name = process.argv[3];
-const number = process.argv[4];
-
 const url = `mongodb+srv://admin:${password}@cluster0.kssm4bs.mongodb.net/phonebook?retryWrites=true&w=majority`;
 
-const phoneSchema = new mongoose.Schema({
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
   name: String,
   number: String,
 });
 
-const Person = mongoose.model("Person", phoneSchema);
-const person = new Person({
-  name: name,
-  number: number,
-});
+const Person = mongoose.model("Person", personSchema);
 
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log("connected");
-    return person.save();
-  })
-  .then((data) => {
-    console.log("person saved! \n", person);
-    return mongoose.connection.close();
-  })
-  .catch((err) => console.log(err));
+if (process.argv.length > 4) {
+  //add person
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+  mongoose
+    .connect(url)
+    .then(() => person.save())
+    .then((pers) => {
+      console.log(`added ${pers.name} number ${pers.number} to phonebook`);
+      mongoose.connection.close();
+    })
+    .catch((error) => console.log(`Error: ${error}`));
+} else {
+  // list all
+  mongoose.connect(url);
+  console.log("phonebook:");
+  Person.find({}).then((persons) => {
+    persons.forEach((person) => console.log(person.name, person.number));
+    mongoose.connection.close();
+  });
+}
